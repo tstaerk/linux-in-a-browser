11aa594296c5f9c9855103ee9cb23f82281081a1
Thorsten Staerk <thorsten@staerk.de>
2026-07-12 18:49:00 +0200

    when clicking onto an icon, do not make its text's background green on purple background

--- start.sh
+++ start.sh
@@ -66,6 +66,7 @@ gsettings set org.mate.background color-shading-type "solid"
 gsettings set org.mate.background primary-color "#000000"
 
 gsettings set org.mate.interface icon-theme 'Papirus'
+gsettings set org.mate.interface gtk-theme 'Arc'
 
 gsettings set org.mate.Marco.general compositing-manager false
 # Run xstartup script manually
